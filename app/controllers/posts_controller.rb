d4c921fd2ce77a9d44317994ec56d00ba366c8d8
Charli Bregnballe <charli@kompjuter.dk>
2016-05-21 20:32:12 +0200

    q git commit -m q

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+	end
+
+	def show
+	end
+
+	def new
+		@post = Post.new
+	end
+
+	def create
+		@post = Post.new(post_params)
+		@post.comments.build
+		@post.user = current_user
+		if @post.save
+			redirect_to root_path
+		else
+			render new
+		end
+	end
+
+
+	private
+
+		def post_params
+			params.require(:post).permit(:content, :user_id, comment_attributes: [:id, :body, :user_id])
+		end
+
+end
